3c6c486c553769423d00e6adafc2a21e0453edd9
Amit Rahav <amit.r.89@gmail.com>
2017-07-14 14:58:35 +0300

    correct varible use at sed command

--- BashAndGulp/create_and_configure_bucket.sh
+++ BashAndGulp/create_and_configure_bucket.sh
@@ -9,14 +9,12 @@ read bucketregion
 echo "creating ${bucketname} bucket, on ${bucketregion}"
 aws s3api create-bucket --bucket ${bucketname} --region ${bucketregion} --create-bucket-configuration LocationConstraint=${bucketregion}
 
-echo "retriving backetname and insert into settings"
-sed -i "s/bucketnameblank/${bucketname}/g" $(pwd)/awss3-policy.conf
-
-echo "this is the policy:"
-cat $(pwd)/*.conf
+echo "create policy by bucket name"
+sed "s|bucketnameblank|${bucketname}|g" $(pwd)/awss3-policy.conf > $(pwd)/${bucketname}-awss3-policy.conf
 
 echo "setting bucket properties to host static website"
-aws s3api put-bucket-website --bucket ${bucketname} --website-configuration file://$(pwd)/awss3-website.conf
+aws s3api put-bucket-website --bucket ${bucketname} --website-configuration file://$(pwd)/${bucketname}-awss3-website.conf
+rm $(pwd)/${bucketname}-awss3-policy.conf
 
 echo "attaching bucket policy"
 aws s3api put-bucket-policy --bucket ${bucketname} --policy file://$(pwd)/awss3-policy.conf
